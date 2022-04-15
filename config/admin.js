664db83392084a5d04e37c426ab58b258676d333
Dao Zheng <daoyzheng@gmail.com>
2022-04-15 16:40:54 -0600

    set apiToken.salt in config admin.js

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,9 @@
 module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET', '38434985c29e0d814ad991c7fed9fd8c'),
+
   },
+  apiToken: {
+    salt: env('API_TOKEN_SALT')
+  }
 });
